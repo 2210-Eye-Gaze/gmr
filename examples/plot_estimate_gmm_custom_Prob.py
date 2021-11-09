bb97f69cdee526b8f7f6ad45bd0a7f1ec80bdb15
danielchen-pyc <daniel7711potter@gmail.com>
2021-11-08 21:30:50 -0800

    Change styling

--- examples/plot_estimate_gmm_custom_Prob.py
+++ examples/plot_estimate_gmm_custom_Prob.py
@@ -68,9 +68,9 @@ X_sampled = gmm.sample(100)
 SAMPLES = 50
 count = 0
 for i in np.linspace(-3,4, SAMPLES):
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(6, 5))
 
-    ax = plt.subplot(1, 2, 1)
+    ax = plt.subplot(1, 1, 1)
     plt.title("Probability Density and Samples")
     plt.xlim((LOWER_LIMIT, UPPER_LIMIT))
     plt.ylim((LOWER_LIMIT, UPPER_LIMIT))
@@ -78,22 +78,21 @@ for i in np.linspace(-3,4, SAMPLES):
     X_test = np.vstack((x.ravel(), y.ravel())).T
     p = gmm.to_probability_density(X_test)
     p = p.reshape(*x.shape)
-    plt.contourf(x, y, p, colors=('#FEE89F', '#D0F87C', '#58F5A9', '#19E5E3', '#41B0F1', '#3C7DE4', '#2B4BE6', '#08249D'))
+    plt.contourf(x, y, p, colors=('#FFFFFF', '#FEE89F', '#D0F87C', '#58F5A9', '#19E5E3', '#41B0F1', '#3C7DE4', '#2B4BE6'))
     s = [2] * 100
     plt.scatter(X_sampled[:, 0], X_sampled[:, 1], c="pink", s=s)
     plt.xlabel('x')
     plt.ylabel('y')
 
-
     interception_x = i
-    plt.axvline(x=interception_x, c='y')
+    # plt.axvline(x=interception_x, c='r')
 
     X_test = np.linspace(LOWER_LIMIT, UPPER_LIMIT, 100)
     cond = gmm.condition(np.array([0]), np.array([i]))
     y = cond.to_probability_density(X_test[:, np.newaxis])
     # print(np.argmax(y)*STEP + LOWER_LIMIT)
     interception_y = np.argmax(y)*STEP + LOWER_LIMIT
-    plt.axhline(y=interception_y, c='orange')
+    # plt.axhline(y=interception_y, c='r')
 
     if prev:
         curr = (interception_x, interception_y)
@@ -110,13 +109,16 @@ for i in np.linspace(-3,4, SAMPLES):
         prev = (interception_x, interception_y)
         
     
-    plt.subplot(1, 2, 2)
-    title = "Conditional PDF $p(y | x = " + str(round(i, 2)) + ")$"
-    plt.title(title)
-    # plt.plot(X_test, y)
-    plt.plot(y, X_test)
-    plt.xlabel('x')
-    plt.ylabel('y')
+    # ax = plt.subplot(1, 2, 2)
+    # title = "Conditional PDF $p(y | x = " + str(round(i, 2)) + ")$"
+    # plt.title(title)
+    # # plt.plot(X_test, y)
+    # # plt.xticks(np.arange(0, 1.0, 0.1))
+    # plt.xlim(0, 0.9)
+    # # ax.xaxis.set_ticks(np.arange(0, 1.0, 0.1))
+    # plt.plot(y, X_test)
+    # plt.xlabel('x')
+    # plt.ylabel('y')
     plt.savefig('Prob/Prob. ' + str(count) + '.png')
     count += 1
     # plt.show()
